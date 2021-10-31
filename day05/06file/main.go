af3a0641c25cd5ce6286097ec0b0fd099f226a10
Tim Zhang <timlinux@qq.com>
2021-10-31 20:14:28 +0800

    bufio, os.Open, file.Read

--- day05/06file/main.go
+++ day05/06file/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
-func main() {
+func osopenread() {
 	// Open 不会创建文件
 	fd, err := os.Open("./main.go")
 	if err != nil {
@@ -14,12 +16,50 @@ func main() {
 	}
 	defer fd.Close()
 
-	var tmp [1024]byte
-	n, err := fd.Read(tmp[:])
+	var tmp [128]byte
+	for {
+		n, err := fd.Read(tmp[:])
+		if err == io.EOF {
+			fmt.Println("read finished(io.EOF)")
+			return
+		}
+
+		if err != nil {
+			fmt.Printf("read from file failed, err:%v\n", err)
+			return
+		}
+		fmt.Printf("read %d bytes\n", n)
+		fmt.Println(string(tmp[:n]))
+		// if n < 128 {
+		// 	fmt.Println("read finished.(size)")
+		// 	return
+		// }
+	}
+}
+
+func bufioread() {
+	fd, err := os.Open("./main.go")
 	if err != nil {
-		fmt.Printf("read from file failed, err:%v\n", err)
+		fmt.Printf("open file faile,d err:%v\n", err)
 		return
 	}
-	fmt.Printf("read %d bytes\n", n)
-	fmt.Println(string(tmp[:n]))
+	defer fd.Close()
+	reader := bufio.NewReader(fd)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			fmt.Println("read file end.(io.EOF)")
+			return
+		}
+		if err != nil {
+			fmt.Printf("read line failed, err:%v\n", err)
+			return
+		}
+		fmt.Print(line)
+	}
+}
+
+func main() {
+	// osopenread()
+	bufioread()
 }
